cd8576ebc892e9988cf5f4c44f423dec3f5babe7
mzaouchi <mzaouchi@gmail.com>
2021-07-09 22:57:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import reactDom from 'react-dom';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+ constructor(props){
+   super(props);
+   this.state={
+     Person:{
+      fullName:"Mahmoud Zaouchi",
+      bio:"I am 27",
+      imgSrc : "/Photo.jpeg",
+      profession :"Developper"
+     },
+
+     shows:true,
+     count :0
+
+   }
+ }
+  componentDidMount(){
+  setInterval(()=>this.setState({count:this.state.count+1}),1000)}
+
+  
+
+ 
+  render(){
+     return(
+       <div style={{margin:"auto",marginTop:"10px",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}> 
+
+      <button onClick={ () =>this.setState({shows : !this.state.shows, count : this.state.count = 0})} style={{backgroundColor:"green",color:"darkgrey",fontSize:"large",fontWeight:"bold"}}>Show</button>
+      
+      {this.state.shows &&
+
+      <div style={{borderStyle:"solid",borderColor:"green",borderRadius:"30px",width:"350px",height:"670px",margin:"auto",marginTop:"15px",display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center"}}>
+      <img src={this.state.Person.imgSrc} alt="" style={{width : "200px", height : "400px"}}/>
+      <h1 style={{color:"darkgreen"}}>{this.state.Person.fullName}</h1>
+      <h2 style={{color:"green"}}>{this.state.Person.bio}</h2>
+      <h3 style={{color:"gray"}}>{this.state.Person.profession}</h3>
+      <h4 style={{color:"darkgreen"}}>{this.state.count}</h4> 
+    </div> 
+      }
+      
+
+      </div>
+    )
+    
+  }
 }
 
 export default App;
